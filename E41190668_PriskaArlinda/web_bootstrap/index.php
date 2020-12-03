3f87975f4fd85b741c7be7166c85ad1a022cab43
priska19 <71807408+priska19@users.noreply.github.com>
2020-12-03 21:19:07 +0700

    login

--- E41190668_PriskaArlinda/web_bootstrap/index.php
+++ E41190668_PriskaArlinda/web_bootstrap/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['email'])){
+if (!isset($_SESSION['username'])){
     header("Location: login_process.php");
 }
 ?>
@@ -192,7 +192,7 @@ if (!isset($_SESSION['email'])){
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['username']; ?> </span>
                 <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
               </a>
               <!-- Dropdown - User Information -->
