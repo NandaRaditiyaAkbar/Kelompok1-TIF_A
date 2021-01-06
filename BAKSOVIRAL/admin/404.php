717944fdb21e4844b2c31f71af3e2d92ef2e3a0e
LigaBayuHerdianto <71804899+LigaBayuHerdianto@users.noreply.github.com>
2021-01-06 21:10:30 +0700

    Update 404.php

--- BAKSOVIRAL/admin/404.php
+++ BAKSOVIRAL/admin/404.php
@@ -145,9 +145,117 @@ $_SESSION['start_time'] = time();
                     <!-- Main row -->
                     <div class="row">
                         <div class="col-lg-12">
-                        <pre>
-                        Menu ini masih dalam proses pembuatan
-                        </pre>
+                       <section>
+        <div class="container">
+        <div class="row">
+         <div class="col-sm-12"> <!--panel-->
+         <h2 class="text-center"><i class="fa fa-list fa-fw"></i> Daftar Konfirmasi Pembayaran</h2>
+            <div class="panel panel-default">
+                 <div class="panel-heading">
+                 <a href="buat_konfirmasi.php" title="Buat Konfirmasi Baru" class="btn btn-primary">Buat Konfirmasi</a>
+                 </div>
+                 <div class="panel-body">
+                 <!-- /.tabel responsive -->
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>No. Transaksi</th>
+                                                <th>Nama Pemilik</th>
+                                                <th>Bank Asal</th>
+                                                <th>Jumlah Pembayaran</th>
+                                                <th>Tanggal Bayar</th>
+                                                <th>Bank Tujuan</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php
+                                        $pg = isset( $_GET['pg'] ) ? $_GET['pg'] : "";
+                                        $batas = 10; /*batas tampilan setiap halaman*/
+                                        if ( empty( $pg ) ) {
+                                        $posisi = 0;
+                                        $pg = 1;
+                                        } else {
+                                        $posisi = ( $pg - 1 ) * $batas;
+                                        }
+                                        /*Jika variabel $pg kosong maka akan menampilkan halaman pertama dengan batas baris 10*/
+
+                                    
+                                       /*mysql_num_rows untuk menghitung total baris di tabel databse*/
+                                        if($jumlah == 0){  //jika tidak ada data
+                                            ?>
+                                        <tr>
+                                            <td colspan="10">Tidak Terdapat Data</td>
+                                        </tr>
+                                        <?php
+                                        }else{
+                                        //jika ada data di tb_brand
+                                        while($a=mysqli_fetch_array($ambildata)){ /*mysql_fetch array untuk mengambil data di setiap field di tabel databse*/
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $posisi=$posisi+1;?></td>
+                                            <td><?php echo $a['id_transaksi'];?></td>
+                                            <td><?php echo $a['nama_pemilik'];?></td>
+                                            <td><?php echo $a['bank_pengirim'];?></td>
+                                            <td><b>Rp. <?php echo number_format($a['jumlah']);?></b></td>
+                                            <td><?php echo tgl_indo($a['tgl_transfer']);?></td>
+                                            <td><?php echo $a['bank_tujuan'];?></td>
+
+                                        </tr>
+                                        <?php
+                                            }
+                                        }
+                                        ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.tabel responsive -->
+
+                                <div class="text-center">
+                                    <?php
+                                //script paging, untuk menampikan setiap halaman
+                                $jml_data = mysqli_num_rows(mysqli_query($conect, "SELECT * FROM tb_konfirmasi order by id_konfirmasi desc"));
+                                $JmlHalaman = ceil($jml_data/$batas); //ceil digunakan untuk pembulatan keatas
+                                if($jml_data != 0){
+                                if ( $pg > 1 ) {
+                                $link = $pg-1;
+                                $prev = "<a href='?pg=$link'><button name='prev' class='btn btn-primary btn-sm'>Prev</button></a> ";
+                                } else {
+                                $prev = "<button name='prev' class='btn btn-default btn-sm'>Prev </button> ";
+                                }
+                                $nmr = '';
+                                for ( $i = 1; $i<= $JmlHalaman; $i++ ){
+
+                                if ( $i == $pg ) {
+                                $nmr .= "<button name='prev' class='btn btn-primary btn-sm'>$i</button> ";
+                                } else {
+                                $nmr .= "<a href='?pg=$i'><button name='prev' class='btn btn-default btn-sm'>$i</button></a> ";
+                                }
+                                }
+                                if ( $pg < $JmlHalaman ) {
+                                $link = $pg + 1;
+                                $next = "<a href='?pg=$link'><button name='prev' class='btn btn-primary btn-sm'>Next</button></a> ";
+                                } else {
+                                $next = "<button name='prev' class='btn btn-default btn-sm'>Next</button> ";
+                                }
+                                echo $prev.$nmr.$next;
+                                ?>
+                                <br><br>
+                                <span class="text-muted">Menampilkan <?php echo $jumlah; ?> dari <?php echo $jml_data; ?> Record </span>
+                                <?php
+                                }
+                                ?>
+                                </div>
+
+
+        </div>
+        </div>
+         </div>
+
+         </div>
+        </div>
+    </section>
           		        </div><!-- col-lg-12--> 
                     </div><!-- /.row (main row) -->
 
